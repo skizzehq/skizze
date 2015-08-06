4c1d0a1dca23db9ac92e3d0b1ccd2fbac3dda3fd
Martin Pinto-Bazurco <martinpintob@gmail.com>
2015-08-06 21:45:28 +0200

    Added a simple log level logger

---
+++ server/log.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"io"
+	"log"
+)
+
+var (
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+/*
+InitLog initializes a simple logging system
+*/
+func InitLog(infoHandle io.Writer, warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
