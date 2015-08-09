bf5014b416118e28fbb4f7d83477b46ecbd3ce4b
Seif Lotfy <seif.lotfy@gmail.com>
2015-08-09 13:00:50 +0200

    Implement save functionality for the storage manager

--- storage/manager.go
+++ storage/manager.go
@@ -1,5 +1,28 @@
 package storage
 
+import (
+	"bufio"
+	"counts/utils"
+	"os"
+	"path/filepath"
+)
+
+// FIXME: path currently hardcoded
+
+func getPath(path string) string {
+	var dataPath, err = filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return dataPath
+}
+
+var dataPath = getPath("~/.counts/storage")
+
+// the storage should deal with 2 types of on disk files, info and data
+// info describes a domain and can be used to load back from disk the settings
+// of a counter to reinitialize it
+// the data is to refill the counters from disk
 type managerStruct struct {
 }
 
@@ -10,6 +33,24 @@ func getManager() *managerStruct {
 		manager = &managerStruct{}
 	}
 	return manager
+}
+
+/*
+Create storage
+*/
+func (m *managerStruct) Create(ID string) {
+	f, err := os.Create(filepath.Join(dataPath, ID))
+	defer f.Close()
+	utils.PanicOnError(err)
+}
+
+func (m *managerStruct) Save(ID string, data []byte) {
+	f, err := os.Create(ID)
+	defer f.Close()
+	utils.PanicOnError(err)
+	w := bufio.NewWriter(f)
+	w.Write(data)
+	w.Flush()
 }
 
 /*
