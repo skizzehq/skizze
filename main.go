b34c6fb6494a1e93b804b0f1bbdef1dbee9125e5
Seif Lotfy <seif.lotfy@gmail.com>
2016-01-07 19:53:09 +0100

    Remove nonsense in main.go

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	_ "net/http/pprof"
 
 	"github.com/seiflotfy/skizze/config"
-	"github.com/seiflotfy/skizze/server"
 	"github.com/seiflotfy/skizze/utils"
 )
 
@@ -16,18 +15,18 @@ var logger = utils.GetLogger()
 
 func main() {
 	var port uint
-	flag.UintVar(&port, "p", 3596, "specifies the port for Counts to run on")
+	flag.UintVar(&port, "p", 3596, "specifies the port for Skizze to run on")
 	flag.Parse()
 
 	//TODO: Add arguments for dataDir and infoDir
 
-	err := os.Setenv("COUNTS_PORT", strconv.Itoa(int(port)))
+	err := os.Setenv("SKIZZE_PORT", strconv.Itoa(int(port)))
 	utils.PanicOnError(err)
 
 	logger.Info.Println("Starting counts...")
 	conf := config.GetConfig()
 	logger.Info.Println("Using data dir: ", conf.DataDir)
-	server, err := server.New()
-	utils.PanicOnError(err)
-	server.Run()
+	//server, err := server.New()
+	//utils.PanicOnError(err)
+	//server.Run()
 }
